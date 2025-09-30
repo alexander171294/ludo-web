58749397c104235ec394099420ba59b5f4541d13
Alexander Eberle <aeberle@tercerpiso.net>
2025-09-30 10:28:05 -0300

    chips

---
+++ frontend/src/app/components/chip/chip.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-chip',
+  standalone: true,
+  imports: [],
+  templateUrl: './chip.component.html',
+  styleUrl: './chip.component.scss'
+})
+export class ChipComponent {
+  @Input() color: 'red' | 'blue' | 'green' | 'yellow' = 'red';
+}
